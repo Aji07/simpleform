6a6821f2276bdaa27b7bf488d97d989302bcef19
Aji07 <70839895+Aji07@users.noreply.github.com>
2023-04-04 16:22:09 +0530

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit} from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'formdesign';
+  index = 0
+// Form Group
+  simpleForm = this.fb.group({
+    userName: ['', RxwebValidators.required()],
+    userEmail: ['', [RxwebValidators.required(), RxwebValidators.email()]],
+    userDob: ['', RxwebValidators.required()],
+    userGender: ['', RxwebValidators.required()]
+  });
+
+  formSubmit = false;
+
+  formData: any = []
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+
+  }
+  get sfc() {
+    return this.simpleForm.controls;
+  }
+  get sfv() {
+    return this.simpleForm.value;
+  }
+  get sfvalid() {
+    return this.simpleForm.valid;
+  }
+
+submitRequest(){
+  if(this.simpleForm.valid){
+    this.formSubmit = true;
+    console.log(this.simpleForm.valid)
+    return;
+  }
+}
+onsubmit(){
+    this.submitRequest();
+    if(this.sfvalid){
+      // console.log(this.sfv);
+      this.formData.push(this.sfv);
+      this.simpleForm.reset();
+    }
+    else{console.log("Form Is invalid", this.sfc)}
+    console.log(this.formData)
+
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
